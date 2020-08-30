92c86f5685e35319d90af522f7a1591a501df37a
pondonwanya <63011060@kmitl.ac.th>
2020-08-30 21:35:34 +0700

    add rectangle

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int  num;
+	scanf("%d", &num);
+	for (int i = 1; i <= num; i++) {
+		for (int j = 1; j <= i; j++) {
+			if (j == 1 || j == i || i == num) {
+				printf("*");
+			}
+			else {
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
